dbb2f368e3712271fd1530b5f92b46a50d325450
Julia Vishnevska <julia.vishnevskaya@hobro.digital>
2026-08-28 17:27:33 +0300

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
             <Route path="/" element={<div>Home</div>} />
             <Route path="/forbidden" element={<div>ForbiddenPage</div>} />
             <Route path="/addquestion" element={<div>AddQuestionPageLazy</div>} />
-            <Route path="/" element={<div>Home</div>} />
           </Route>
         </Routes>
       </BrowserRouter>
