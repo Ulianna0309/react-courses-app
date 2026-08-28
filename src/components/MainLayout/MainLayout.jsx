32956f995e6e38a0e270dc9dfef2665e80f3b0f0
Julia Vishnevska <julia.vishnevskaya@hobro.digital>
2026-08-28 17:27:12 +0300

    added router

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import styles from "./MainLayout.module.css";
+
+export const MainLayout = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className={styles.mainLayout}>
+      <header>header</header>
+      <div className={styles.mainWrapper}>
+        <main className={styles.main}>
+            <Outlet/>
+        </main>
+        <footer className={styles.footer}>
+          React Question Cards Application | {currentYear} <br />
+          by Julia V.
+        </footer>
+      </div>
+    </div>
+  );
+};
