0993d82f787c5498c1b0e28b2938915f11cc1ed6
Julia Vishnevska <julia.vishnevskaya@hobro.digital>
2026-09-02 23:13:30 +0300

    adde hook delayFn

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,27 +2,28 @@ import { QuestionCardList } from "../../components/QuestionCardList";
 import styles from "./HomePage.module.css";
 import { API_URL } from "../../constants";
 import { useEffect, useState } from "react";
+import { Loader } from "../../components/Loader/Loader";
+import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions, error] = useState([]);
+
+  const [getQuestions, isLoading] = useFetch(async (url) => {
+    const response = await fetch(`${API_URL}/${url}`);
+    const questions = await response.json();
+    setQuestions(questions);
+    return questions;
+  });
 
   useEffect(() => {
-    const getQuestions = async () => {
-      try {
-        const response = await fetch(`${API_URL}/react`);
-        const data = await response.json();
-        setQuestions(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getQuestions();
+    getQuestions("react");
   }, []);
 
   return (
     <div className={styles.HomePage}>
-      <QuestionCardList cards={questions}/>
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
+      <QuestionCardList cards={questions} />
     </div>
   );
 };
