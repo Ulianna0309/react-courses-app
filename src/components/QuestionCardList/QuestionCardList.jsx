a1f385cb3eb170494ff5844ed19454f6fbbe4793
Julia Vishnevska <julia.vishnevskaya@hobro.digital>
2026-09-13 20:56:26 +0300

    input, memo

--- src/components/QuestionCardList/QuestionCardList.jsx
+++ src/components/QuestionCardList/QuestionCardList.jsx
@@ -1,12 +1,13 @@
+import { memo } from "react";
 import { QuestionCard } from "../../components/QuestionCard";
 import styles from "./QuestionCardList.module.css";
 
-export const QuestionCardList = ({cards}) => {
+export const QuestionCardList = memo(({ cards }) => {
   return (
     <div className={styles.cardList}>
-        {cards.map((card) => (
+      {cards.map((card) => (
         <QuestionCard card={card} key={card.id} />
       ))}
     </div>
   );
-};
+});
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,6 +7,7 @@ import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
   const [questions, setQuestions, error] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const [getQuestions, isLoading] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
@@ -19,8 +20,15 @@ export const HomePage = () => {
     getQuestions("react");
   }, []);
 
+  const onSeachChangeHandler = (e) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value)
+  }
+
   return (
     <div className={styles.HomePage}>
+      <input type="text" placeholder="search" value={searchValue} onChange={onSeachChangeHandler}/>
+      {/* <button onClick={testRefHendler}>test ref</button> */}
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
       <QuestionCardList cards={questions} />
